b8317d15e1e042ee74bb83dad1a577e222b05647
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2006-03-10 15:48:39 +0000

    documentation; refactored sbcl-isms
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@59 97840717-62f2-0310-bead-daafb7b60902

--- src/rclg-init.lisp
+++ src/rclg-init.lisp
@@ -1,3 +1,19 @@
+;;; RCLG: R-CommonLisp Gateway
+
+;;; Copyright (c) --2006, rif@mit.edu.  All Rights Reserved.
+;;; Author: rif@mit.edu
+;;; Maintainers: rif@mit.edu, AJ Rossini <blindglobe@gmail.com>
+;;; License:
+
+;;; Intent: R evaluator process initialization and maintenance.  A
+;;; good deal of this is SBCL specific, and is flagged if so.  At some
+;;; point, a means of handling non-SBCL variants will be important.
+;;; But first things first.
+
+;;; Basic Usage:
+;;; (start-rclg) ;; initializes RCLG functions.
+;;; 
+
 (defpackage :rclg-init
   (:use :common-lisp :rclg-foreigns :cffi)
   (:export :start-rclg :with-R-traps :update-R 
@@ -7,21 +23,31 @@
 
 (in-package :rclg-init)
 
+;; initialization
 (defvar *r-default-argv* '("rclg" "-q" "--vanilla"))
 (defvar *r-started* nil)
+
+;; thread management
 (defvar *do-rclg-updates-p* nil)
 (defvar *rclg-update-sleep-time* .1)
 (defvar *rclg-update-mutex* (sb-thread:make-mutex))
 
+#+sbcl
 (defmacro with-r-traps (&body body)
+  "Protect against R signaling wierdness.  This is mostly for
+initializing the R REPL."
   `(sb-int:with-float-traps-masked  (:invalid :divide-by-zero)
     ,@body))
 
+#+sbcl
 (defmacro with-r-mutex (&body body)
+  "FIXME:AJR: eval body in an mutex thread, updating as necessary.  AJR is
+not clear about the use-case for this macro."  
   `(sb-thread:with-mutex (*rclg-update-mutex*)
     ,@body))
 
 (defun update-R ()
+  "Update and sync all SBCL threads containing R processes." 
   (with-r-traps
     (with-r-mutex
       (%r-run-handlers *r-input-handlers*
@@ -29,6 +55,8 @@
 
 #+sbcl
 (defun start-rclg-update-thread ()
+  "Update R threads. FIXME:AJR add use case when/if this is needed at
+a user-level."
   (setf *do-rclg-updates-p* t)
   (sb-thread:make-thread 
    #'(lambda ()
@@ -38,20 +66,28 @@
 	       (update-R)
 	       (sleep *rclg-update-sleep-time*))))))
 
-#+sbcl
+
 (defun stop-rclg-update-thread ()
+  "FIXME: this was initially flagged as SBCL only, but it is more
+generic.  However, it isn't useful until implemented on other CL
+systems." 
   (setf *do-rclg-updates-p* nil))
 	       
 
 (defun string-sequence-to-foreign-string-array (string-sequence)
+  "CFFI-based conversion.  Isn't there a new CFFI function for this?
+FIXME:AJR: need to check."
   (let ((n (length string-sequence)))
     (let ((foreign-array (foreign-alloc :pointer :count n)))
       (dotimes (i n)
  	(setf (mem-aref foreign-array :pointer i)
 	      (foreign-string-alloc (elt string-sequence i))))
-      (values foreign-array n))))
+      (values foreign-array n)))) 
 
-(defmacro with-foreign-string-array ((name length str-array) &body body)
+(defmacro with-foreign-string-array
+    ((name length str-array) &body body)
+  "CFFI-based conversion.  Isn't this implemented in CFFI? FIXME:AJR:
+need to check."
   (let ((ctr (gensym)))
     `(multiple-value-bind (,name ,length) 
       (string-sequence-to-foreign-string-array ,str-array)    
@@ -65,6 +101,7 @@
 
 
 (defun start-rclg (&optional (argv *r-default-argv*))
+  "Initial the first R thread, perhaps with different arguments."  
   (unless *r-started*
     (progn
       #+sbcl(sb-int:set-floating-point-modes :traps (list :overflow))
@@ -74,5 +111,7 @@
 		(%rf-init-embedded-r n foreign-argv))
 	      #+sbcl(start-rclg-update-thread))))))
 
+;;; Do we really want to force this, or should we wait and let the
+;;; user do this action? 
 (eval-when (:load-toplevel)
   (start-rclg))
