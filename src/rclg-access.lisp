f8ba02e023f84565439d0fff61d0a000a5da9222
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2006-03-10 15:46:32 +0000

    documentation additions
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@57 97840717-62f2-0310-bead-daafb7b60902

--- src/rclg-access.lisp
+++ src/rclg-access.lisp
@@ -1,3 +1,13 @@
+;;; RCLG: R-CommonLisp Gateway
+
+;;; Copyright (c) --2006, rif@mit.edu.  All Rights Reserved.
+;;; Author: rif@mit.edu
+;;; Maintainers: rif@mit.edu, AJ Rossini <blindglobe@gmail.com>
+;;; License:
+
+;;; Intent: This code supports interaction between R's SEXPs and CL. 
+;;; FIXME:AJR:  worth having a CLOSy style interface?
+
 (defpackage :rclg-access
   (:use :common-lisp :cffi :rclg-types)
   (:export :r-setcar :r-car :r-cdr))
@@ -5,9 +15,11 @@
 (in-package :rclg-access)
 
 (defun r-get-union (sexp)
+  "returns a component of an R SEXP."
   (foreign-slot-value sexp 'sexprec 'sxp-int-union))
 
 (defun r-get-listsxp (sexp)
+  "returns an R LIST SEXP."
   (foreign-slot-value 
    (r-get-union sexp) 
    'sexprec-internal-union 
