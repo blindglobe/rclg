83bd75cfe0a56e7fe523d2b38bf69fa6051bb9d5
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2006-04-11 20:14:04 +0000

    put rclg-load.lisp back into compilable state
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@113 97840717-62f2-0310-bead-daafb7b60902

--- src/rclg-load.lisp
+++ src/rclg-load.lisp
@@ -16,27 +16,37 @@
 (in-package :rclg-load)
 
 (defvar *rclg-loaded* nil
-  "True once rclg is loaded, nil while still loading.")
+  "True once rclg is loaded, nil otherwise (including errors).")
 
-(defvar *r-home*  ;; change to *r-home* path?   What should be done here?
+(defvar RCLG-home-string
+  "/home/rossini/public_html/CLS/CommonLispStat/CLS1.0A1.lisp/LispPackages/RCLG.svn"
+  "String containing location of RCLG directory.
+(or other place to have help compiled code libraries).")
+
+(defvar r-home-string "/opt/rdevel/lib/R"
+  "String containing R_HOME.")
+
+(defvar *r-home* (pathname r-home-string)
   ;; #p"/home/rif/software/R-2.2.1/"
-  ;; #p"/opt/Rdevel"
-  ;; #p"/usr/lib/R"
-  (pathname "/usr/lib/R")
-  "This variable defines R-HOME (root installation directory).
+  "This pathname variable points to R-HOME (root installation directory).
 Needs to be configured by the user, or self-discovered.  See R docs
 for more details.")
 
-;(defvar r-ld-library-additions-string
-;  ( ))
+(defvar r-ld-library-additions-root-string '("/lib" "/library/grDevices/libs/")
+  "Strings containing paths to libraries to put on LD_LIBRARY_PATH for loading.")
 
-(defvar *r-ld-library-additions*
+;(defvar r-ld-library-additions-string
+;  (concatenate 'string  r-home-string  r-ld-library-additions-root-string)
+;  "Strings containing paths to libraries to put on LD_LIBRARY_PATH for loading.")
+
+
+;(defvar *r-ld-library-additions*
 ;  '(#p"/usr/lib/R/lib"
 ;    #p"/usr/lib/R/library/grDevices/libs/"))
-  (mapcar #'pathname (concatenate 'string
-				  (mapcar #'namestring r-ld-library-additions-string)
-				  #p"/opt/rdevel/lib/R/lib"
-    #p"/opt/rdevel/lib/R/library/grDevices/libs/")))
+;  (mapcar #'pathname (concatenate 'string
+;				  (mapcar #'namestring r-ld-library-additions-string)
+;				  #p"/opt/rdevel/lib/R/lib"
+;    #p"/opt/rdevel/lib/R/library/grDevices/libs/")))
 
 ;; FIXME:AJR: (AJR needs more lisp-fu):
 ;; ERROR - these are paths, not strings!
@@ -79,9 +89,15 @@ for more details.")
 
 ;;(define-foreign-library libR (t (:default "libR")))
 ;;(define-foreign-library libRg (t (:default "grDevices")))
-(define-foreign-library libR (t (:default "/usr/lib/R/lib/libR")))
-(define-foreign-library libRg (t (:default "/usr/lib/R/library/grDevices/libs/grDevices")))
 
+;(define-foreign-library libR (t (:default "/usr/lib/R/lib/libR")))
+;(define-foreign-library libRg (t (:default "/usr/lib/R/library/grDevices/libs/grDevices")))
+
+;(define-foreign-library libR (t (:default "/opt/rdevel/lib/R/lib/libR")))
+;(define-foreign-library libRg (t (:default "/opt/rdevel/lib/R/library/grDevices/libs/grDevices")))
+
+(define-foreign-library libR (t (:default "/opt/R-2-2-patches/lib/R/lib/libR")))
+(define-foreign-library libRg (t (:default "/opt/R-2-2-patches/lib/R/library/grDevices/libs/grDevices")))
 
 ;; FIXME:AJR: Configuration issue: Figure out libR location from
 ;; R_HOME, and the RCLG location, and insert.  This is a hack. 
@@ -90,8 +106,10 @@ for more details.")
 (unless *rclg-loaded*
   (let ((*foreign-library-directories*
  	 (append (list #p"/home/rossini/public_html/CLS/CommonLispStat/CLS1.0A1.lisp/LispPackages/RCLG.svn/c/"
-		       #p"/opt/rdevel/lib/R/lib/"
-		       #p"/opt/rdevel/lib/R/library/grDevices/libs/"
+		       ;; #p"/opt/rdevel/lib/R/lib/"
+		       ;; #p"/opt/rdevel/lib/R/library/grDevices/libs/"
+		       #p"/opt/R-2-2-patches/lib/R/lib"
+		       #p"/opt/R-2-2-patches/lib/R/library/grDevices/libs/"
 		       ;; #p"/usr/lib/R/lib/"
 		       ;; #p"/usr/lib/R/library/grDevices/libs/"
 		       ;; #p"/home/rif/Projects/RCLG/c/"
@@ -113,23 +131,22 @@ for more details.")
 			     ;;"/usr/lib/R/lib:"
 			     ;;"/usr/lib/R/library/grDevices/libs/:"
 			     ;; Verify, --prefix=/opt/rdevel
-			     "/opt/rdevel/lib:"
-			     "/opt/rdevel/lib/R/library/grDevices/libs/:"
+			     "/opt/R-2-2-patches/lib:"
+			     "/opt/R-2-2-patches/lib/R/library/grDevices/libs/:"
 			     (posix-getenv "LD_LIBRARY_PATH"))
 		1)
 
-  (pushnew #P"/opt/rdevel/lib/R/lib/"
+  (pushnew #P"/opt/R-2-2-patches/lib/R/lib/"
            ;; #P"/usr/lib/R/lib/"
 	   *foreign-library-directories*
 	   :test #'equal)
 
-  (pushnew #P"/opt/rdevel/lib/R/library/grDevices/libs/"
+  (pushnew #P"/opt/R-2-2-patches/lib/R/library/grDevices/libs/"
 	   ;; #P"/usr/lib/R/library/grDevices/libs/"
 	   *foreign-library-directories*
 	   :test #'equal)
 
-  (define-foreign-library libR
-    (t (:default "libR")))
+  (define-foreign-library libR (t (:default "libR")))
 
   (use-foreign-library libR)
 
