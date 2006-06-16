d63be27ffa92adbc1b318b47fa90853168b03e3c
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2006-06-16 16:01:00 +0000

    added test-demos that need to be do-able
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@264 97840717-62f2-0310-bead-daafb7b60902

--- rclg-demo.lisp
+++ rclg-demo.lisp
@@ -7,8 +7,9 @@
 ;; if needed...?  Shouldn't be, since rclg.asd ought to take care of
 ;; most of the issues that we have.
 (asdf:operate 'asdf:compile-op 'cffi)
-(asdf:operate 'asdf:load-op 'cffi)
 (asdf:operate 'asdf:compile-op 'rclg)
+
+(asdf:operate 'asdf:load-op 'cffi)
 (asdf:operate 'asdf:load-op 'rclg)
 
 ;;;#2 Go to where the functions are 
@@ -51,32 +52,57 @@
 
 ;; These don't work if we have library problems.
 (r "library" "stats") 
-(r "library" "MASS")
+(r library "MASS")
 (r "library" "Biobase") 
+
+(setf my.lib "Biobase")
+my.lib
+(r library my.lib)
 
 (r "ls")
 
 (r assign "x" 5)
+(r assign "x2" (list 1 2 3 5))
+
+(r assign "x2" #(1 2 3 5 3 4 5))
 (r assign "z" "y")
 
 (r "get" "x")
+(r get "x2")
 (r "get" "z")
 (r get "z")
 (r "ls")
 
-;;; Not quite yet; we need vectors and dataframe conversion
-(r "assign" "my.x" (r "rnorm" 10))
+
+(r assign "my.x" (r rnorm 10)) ;; fails only because we don't have the
+;; translation tools working.
+(r assign "my.x" (rnb rnorm 10)) ;; works at this point.
+
 (r get "my.x")
 
 (r "plot" #(2 3 3 2 1) #(3 5 7 3 2)) ; works
 
 (r plot (list 1 2 3 4 5) (list 1 2 3 4 5) :main "My title")
+(r plot :x (list 1 2 3 4 5) :y (list 5 4 3 4 5) :main "My title")
 
-(r plot (list (r rnorm 10)) (list (r rnorm 10)) :main "silly")
+(r plot :y (list 5 4 3 4 5)  :x (list 1 2 3 4 5) :main "My title")
+
+(r plot (rnb rnorm 10) (rnb rnorm 10)
+   :main "silly" :xlab "xlabel" :ylab "ylabel")
+
+;;; Need to be able to run!
+(r assign "my.df" (r read.table "testdata.csv"))
+(r summary "my.df")
+
+;;; How to handle connections?
+
+;;; How to handle S4 objects?
+;;; Hook into the conversion?
 
 ;;; how do we terminate the R session?
 (r "q" "y")
 
+(aref (r rnorm 10) 3)
 
 ;;;#5 Old useless code.
 
