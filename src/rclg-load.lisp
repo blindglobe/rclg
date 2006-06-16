d98433c9d3fe587f966c6ca47e4f73fef29d293c
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2006-06-16 10:05:12 +0000

    updated docs, kept rif's path the right one
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@257 97840717-62f2-0310-bead-daafb7b60902

--- src/rclg-load.lisp
+++ src/rclg-load.lisp
@@ -19,17 +19,39 @@
   "True once rclg is loaded, nil otherwise (including errors).")
 
 ;; The user MUST make sure *R-HOME-STR* points to the right place!  --rif
+;; Need to fix this by flagging RIF or TONY or someone else.
+
+;;; This should be fixed.
 (eval-when (:compile-toplevel :load-toplevel)
-  (defvar *R-HOME-STR* "/home/rif/rclg-test/R-2.3.1"))
+  (defvar *R-HOME-STR* 
+    ;;"/home/rif/RCLG-test/R-2.3.1"
+    "/home/rif/rclg-test/R-2.3.1"
+    ;;"/usr/lib/R"             ;; root is /usr/
+    ;;"/opt/R-2-2-patches/lib/R" ;; root is /opt/R-2-2-patches/
+    ;;"/opt/R-2-3-patches/lib/R" ;; root is /opt/R-2-3-patches/
+    ;;"/opt/rdevel/lib/R"        ;; root is /opt/rdevel/
+    ))
+
+;; (setf *R-HOME-STR*   "/usr/lib/R")
+;; *R-HOME-STR*
+
+(defvar *R-CORE-LIB-DIRS*
+  (list (concatenate 'string *R-HOME-STR* "/lib")
+	(concatenate 'string *R-HOME-STR* "/library/grDevices/libs/")))
+
+;; (setf *R-CORE-LIB-DIRS*
+;;        (list (concatenate 'string *R-HOME-STR* "/lib")
+;;              (concatenate 'string *R-HOME-STR* "/library/grDevices/libs/")))
+;; *R-CORE-LIB-DIRS*
 
 (posix-setenv "R_HOME" *R-HOME-STR* 1)
 
 (defun load-r-libraries () 
   (unless *rclg-loaded*
     (progn
-      (define-foreign-library libR (t (:default #.(concatenate 'string *R-HOME-STR* "/lib/libR"))))
+      (define-foreign-library libR
+	  (t (:default #.(concatenate 'string *R-HOME-STR* "/lib/libR"))))
       (use-foreign-library libR)
       (setf *rclg-loaded* t))))
 
 ;; *rclg-loaded*
-  
