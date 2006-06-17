d2e8203b15a86977d0f424d26ec6d1d1066abf8f
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2006-06-17 08:07:21 +0000

    incorporated rif's explanation of the r, rnb, rnbi functions into the demo file
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@267 97840717-62f2-0310-bead-daafb7b60902

--- rclg-demo.lisp
+++ rclg-demo.lisp
@@ -36,8 +36,64 @@
 
 ;;;#4 Demonstration of commands
 
-(r "print.default" 3)
-(r "rnorm" 10)
+
+ 
+;; Basically, you now have three choices:
+;; 
+;; r   --- calls R, and converts the result back to CL as best as it can.  If it can't convert, returns an unprotected
+;;         sexp (probably a bug, probably should be protected)
+;; 
+;; rnb --- R no backconvert.  Calls R, and returns a protected unconverted R sexp.  Useful when you want to
+;;         manipulate something on the R side and give it a CL name
+;; 
+;; rnbi --- R no backconvert internal.  Calls R, returns a protected uncoverted R sexp.  However, it's
+;;          tagged differently, and as soon as you use this as an argument to a function, it unprotects
+;;          the sexp.  Useful for holding anonymous intermediate R results you don't want to backconvert.
+;; 
+;; Protection/unprotection controls whether R can GC the sexp.
+;; 
+;; Example: 
+;; 
+;; CL-USER> (defparameter *x* (r seq 1 10))
+;; *X*
+;; CL-USER> (defparameter *y* (rnbi rnorm 10))
+;; *Y*
+;; CL-USER> *y*
+;; #<sexp at 0x89A0238, PROTECT=R-PROTECT-UNTIL-USED>
+;; CL-USER> (r plot *x* *y*)
+;; NIL
+;; NIL
+;; CL-USER> *y*
+;; #<sexp at 0x89A0238, PROTECT=NIL>
+
+;; code used above
+(defparameter *x* (r seq 1 10))
+(defparameter *y* (rnbi rnorm 10))
+*y*
+(r plot *x* *y*)
+*y*
+
+;; This is for illustrative purposes only.  It is not a "good" use of rnbi.
+;; Really, you'll want rnbi to hold anonymous intermeditae results, like:
+
+(r plot *x* (rnbi rnorm 10))
+
+;; Notes:
+;; 
+;; If the user protects the result of a call with rnb, it is the
+;; user's responsibility to delete the sexp when it's no longer needed,
+;; using rclg-control:unprotect-sexp.  (It might be better to use a
+;; modification of the old safe version that's lying around.)
+;; 
+;; There is no way to ask R whether an sexp is protected or not.
+;; Therefore, there is no real way to enforce the protection.  If the user
+;; goes around the API and calls %rf-unprotect-ptr or messes with the
+;; description slot (slot-value sexp-holder 'protected), things can easily
+;; get out of sync.
+
+
+;; Examples of function use:
+
 
 (r "Sys.getenv" "LD_LIBRARY_PATH")
 (r "Sys.getenv" "LD_PRELOAD")
@@ -45,10 +101,8 @@
 (r "ls")
 (r "search") ;; works
 
-;(r "a <- 1") ;; should not work -- what is the right way to assign
-	     ;; variables in R?  We could use the function call,
-	     ;; i.e. something like (r "assign" "y" 10), but that
-	     ;; doesn't quite seem right.
+(r "geterrmessage")
+
 
 ;; These don't work if we have library problems.
 (r "library" "stats") 
@@ -61,11 +115,19 @@ my.lib
 
 (r "ls")
 
+(r "print.default" 3)
+(r "rnorm" 10)
+
+;; Working in the R space
+
 (r assign "x" 5)
 (r assign "x2" (list 1 2 3 5))
 
 (r assign "x2" #(1 2 3 5 3 4 5))
 (r assign "z" "y")
+
+(setf my.r.x2 (r get "x2"))  ;; moving data from R to CL
+(r assign "x2" my.r.x2)  ;; moving data from CL to R
 
 (r "get" "x")
 (r get "x2")
@@ -79,6 +141,9 @@ my.lib
 (r assign "my.x" (rnb rnorm 10)) ;; works at this point.
 
 (r get "my.x")
+
+
+;; More sophisticated computation
 
 (r "plot" #(2 3 3 2 1) #(3 5 7 3 2)) ; works
 
@@ -127,6 +192,25 @@ my.lib
   (rclg-control::rname-to-robj "runif")
   (rclg-control::rname-to-rfun "runif")
   (rclg-control::rname-to-rfun "search"))
+
+#|
+On 6/16/06, rif <rif@mit.edu> wrote:
+> 
+> OK, I took a stab at the conversion business, and just checked in my attempt.
+> 
+
+
+-- 
+best,
+-tony
+
+blindglobe@gmail.com
+Muttenz, Switzerland.
+"Commit early,commit often, and commit in a repository from which we can easily
+roll-back your mistakes" (AJR, 4Jan05).
+
+|#
+
 
 ;;; Local variables:
 ;;; mode: outline-minor
