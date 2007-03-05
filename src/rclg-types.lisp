0e369494b1e64fd03be9787c5230e7a0abe666c9
rossini <rossini@97840717-62f2-0310-bead-daafb7b60902>
2007-03-05 19:14:26 +0000

    Copyright mod'ed to BSD style.
    git-svn-id: https://middleangle.com/svn/rif/projects/RCLG@621 97840717-62f2-0310-bead-daafb7b60902

--- src/rclg-types.lisp
+++ src/rclg-types.lisp
@@ -1,8 +1,38 @@
 ;;; RCLG: R-CommonLisp Gateway
 
-;;; Copyright (c) --2006, rif@mit.edu.  All Rights Reserved.
-;;; Author: rif@mit.edu
-;;; Maintainers: rif@mit.edu, AJ Rossini <blindglobe@gmail.com>
+;;; Copyright (c) 2005--2007, <rif@mit.edu>
+;;;                           AJ Rossini <blindglobe@gmail.com>
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are
+;;; met:
+;;;
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above
+;;;       copyright notice, this list of conditions and the following disclaimer
+;;;       in the documentation and/or other materials provided with the
+;;;       distribution.
+;;;     * The names of the contributors may not be used to endorse or
+;;;       promote products derived from this software without specific
+;;;       prior written permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+;;; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+;;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;;; Author:      rif@mit.edu
+;;; Maintainers: rif@mit.edu,
+;;;              AJ Rossini <blindglobe@gmail.com>
 
 ;;; Intent: Data structures in CommonLisp for representing R internal
 ;;; data structures.
@@ -77,8 +107,8 @@
 ;;; FIXME:AJR:  Can we use a FFI call as an alternative to return the
 ;;; type?  It would be portable, but it would not be fast.  ARGH.
 (defun sexptype (robj)
-  "Gets the sexptype of an robj.  WARNING: 
-ASSUMES THAT THE TYPE IS STORED IN THE LOW ORDER 5 BITS OF THE
+  "Gets the sexptype of an robj.
+WARNING: ASSUMES THAT THE TYPE IS STORED IN THE LOW ORDER 5 BITS OF THE 
 SXPINFO-STRUCT, AND THAT IT CAN BE EXTRACTED VIA A 'mod 32' OPERATION!
 MAY NOT BE PORTABLE."
   (let ((info (foreign-slot-value
